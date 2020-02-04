f2351a3bafbbc7b5b364d81f548ec52682da9287
Dusan Radovanovic <dusan.radovanovic@elasticpath.com>
2020-02-04 12:20:33 -0800

    Initial commit from Create React App

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+const App: React.FC = () => {
+  return (
+    <div className="app-component">
+      Minimal Typescript/SCSS based template
+    </div>
+  );
+}
+
+export default App;
