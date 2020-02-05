c2eea87c3bcf1b5578ef5720ecd567c114526ff9
Dusan Radovanovic <dusan.radovanovic@elasticpath.com>
2020-02-05 15:25:08 -0800

    Pre-prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
 import './App.scss';
 
+interface ButtonProps {
+  isPrimary: boolean;
+}
+
+const Button = styled.button<ButtonProps>`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+  cursor: pointer;
+  outline: none;
+
+  &:hover {
+    box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.2);
+  }
+
+  ${props => props.isPrimary &&
+    css`
+      background: palevioletred;
+      color: white;
+    `};
+`;
+
 const App: React.FC = () => {
+  const [isPrimary, setIsPrimary] = useState();
+
+  const handleClick = () => {
+    setIsPrimary(!isPrimary);
+  };
+
   return (
     <div className="app-component">
       Minimal Typescript/SCSS based template
+
+      <Button isPrimary={isPrimary} onClick={() => handleClick()}>Click Me!</Button>
+
     </div>
   );
 }
